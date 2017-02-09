3daf382d5ac27aadcd833390a4d9fb27cb1484c8
Donghdd00342 <donghdd00342@fpt.edu.vn>
2017-02-09 15:01:24 +0700

    Copy Constructor...

--- itest/src/itest/Itest.java
+++ itest/src/itest/Itest.java
@@ -37,14 +37,68 @@ package itest;
 //        s2.display();
 //    }
 //}
-class Student4 {
+//class Student4 {
+//
+//    int id;
+//    String name;
+//
+//    Student4(int i, String n) { // Constructor
+//        id = i;
+//        name = n;
+//    }
+//
+//    void display() {
+//        System.out.println(id + " " + name);
+//    }
+//
+//    public static void main(String args[]) {
+//        Student4 s1 = new Student4(111, "humty");
+//        Student4 s2 = new Student4(222, "dumty");
+//        s1.display();
+//        s2.display();
+//    }
+//}
+//class Student9 { // nạp chồng Constructor 
+//
+//    int id;
+//    String name;
+//    int age;
+//
+//    Student9(int i, String n) {
+//        id = i;
+//        name = n;
+//    }
+//
+//    Student9(int i, String m, int a) {
+//        id = i;
+//        name = m;
+//        age = a;
+//    }
+//
+//    void display() {
+//        System.out.println(id + " " + name + " " + age);
+//    }
+//
+//    public static void main(String args[]) {
+//        Student9 s1 = new Student9(111, "Hoang");
+//        Student9 s2 = new Student9(222, "Thanh", 25);
+//        s1.display();
+//        s2.display();
+//    }
+//}
+class Student6 { // Copy Constructor
 
     int id;
     String name;
 
-    Student4(int i, String n) { // Constructor
+    Student6(int i, String n) {
         id = i;
         name = n;
+    }
+
+    Student6(Student6 s) {
+        id = s.id;
+        name = s.name;
     }
 
     void display() {
@@ -52,8 +106,8 @@ class Student4 {
     }
 
     public static void main(String args[]) {
-        Student4 s1 = new Student4(111, "humty");
-        Student4 s2 = new Student4(222, "dumty");
+        Student6 s1 = new Student6(111, "Hoang");
+        Student6 s2 = new Student6(s1);
         s1.display();
         s2.display();
     }
