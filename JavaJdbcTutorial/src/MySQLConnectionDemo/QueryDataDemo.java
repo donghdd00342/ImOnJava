7178d0a600167029982cbcc0222f4194fe5efa08
Donghdd00342 <donghdd00342@fpt.edu.vn>
2017-02-12 18:58:27 +0700

    Xem kết quả trả về (dữ liệu) với đối tượng ResultSet....

--- JavaJdbcTutorial/src/MySQLConnectionDemo/QueryDataDemo.java
+++ JavaJdbcTutorial/src/MySQLConnectionDemo/QueryDataDemo.java
@@ -28,17 +28,36 @@ public class QueryDataDemo {
         // III. Khởi tạo một string để query
         String sql;
         // III.1. Ví dụ tạo bảng
-        sql = "CREATE TABLE students ("
-            + "rollno INTEGER NOT NULL,"
-            + "name VARCHAR(255) NOT NULL,"
-            + "phonenumber VARCHAR(20));";
-        // Thực thi câu lệnh.
-        // executeUpdate(String) sử dụng cho các loại lệnh Create,Insert,Update,Delete...
-        statement.executeUpdate(sql);
-        System.out.println("Tạo bảng thành công");
+//        sql = "CREATE TABLE students ("
+//            + "rollno VARCHAR(20) NOT NULL,"
+//            + "name VARCHAR(255) NOT NULL,"
+//            + "phonenumber VARCHAR(20));";
+//        // Thực thi câu lệnh.
+//        // executeUpdate(String) sử dụng cho các loại lệnh Create,Insert,Update,Delete...
+//        statement.executeUpdate(sql);
+//        System.out.println("Tạo bảng thành công");
         // III.2 Ví dụ Insert dữ liệu^
-//        sql = "INSERT INTO table_name (column1,column2,column3,...)\n"
-//            + "VALUES (value1,value2,value3,...);";
+//        sql = "INSERT INTO students (rollno,name,phonenumber)"
+//            + "VALUES ('HDD00342', 'HoDong', '0979586328');";
+//        // Thực thi câu lệnh.        
+//        statement.executeUpdate(sql);
+//        System.out.println("Thêm dữ liệu thành công");
+        // III.3 Xem kết quả trả về (dữ liệu) với đối tượng ResultSet.
+        sql = "SELECT * FROM students";
+        // Thực thi câu lệnh SQL trả về đối tượng ResultSet.
+        ResultSet rs = statement.executeQuery(sql);
+        // Duyệt trên kết quả trả về.
+        while (rs.next()) {// Di chuyển con trỏ xuống bản ghi kế tiếp.
+            String rollNo = rs.getString("rollno");
+            String name = rs.getString("name");
+            String phoneNumber = rs.getString("phonenumber");
+            System.out.println("--------------------");
+            System.out.println("Số hiệu:" + rollNo);
+            System.out.println("Họ Tên:" + name);
+            System.out.println("Số điện thoại:" + phoneNumber);
+        }
+        // Đóng kết nối
+        connection.close();
 
     }
 
