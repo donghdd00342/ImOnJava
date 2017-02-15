6122530c0b90be8f0febecb2a6e78bfdf3358785
Donghdd00342 <donghdd00342@fpt.edu.vn>
2017-02-15 14:30:19 +0700

    Thử commend-line....

--- iTest/src/itest/Itest.java
+++ iTest/src/itest/Itest.java
@@ -373,41 +373,51 @@ package itest;
 //    }
 //}
 ////////// Ví dụ Overloading
-class ExampleOverloading {
+//class ExampleOverloading {
+//
+//    public static void main(String[] args) {
+//        int a = 11;
+//        int b = 6;
+//        double c = 7.3;
+//        double d = 9.4;
+//        int result1 = minFunction(a, b);
+//        // cung ten ham voi tham so khac nhau
+//        double result2 = minFunction(c, d);
+//        System.out.println("Gia tri nho nhat = " + result1);
+//        System.out.println("Gia tri nho nhat = " + result2);
+//    }
+//
+//    // cho integer
+//    public static int minFunction(int n1, int n2) {
+//        int min;
+//        if (n1 > n2) {
+//            min = n2;
+//        } else {
+//            min = n1;
+//        }
+//
+//        return min;
+//    }
+//    // cho double
+//
+//    public static double minFunction(double n1, double n2) {
+//        double min;
+//        if (n1 > n2) {
+//            min = n2;
+//        } else {
+//            min = n1;
+//        }
+//
+//        return min;
+//    }
+//}
+////////// Thử commend-line
+public class Itest {
 
-    public static void main(String[] args) {
-        int a = 11;
-        int b = 6;
-        double c = 7.3;
-        double d = 9.4;
-        int result1 = minFunction(a, b);
-        // cung ten ham voi tham so khac nhau
-        double result2 = minFunction(c, d);
-        System.out.println("Gia tri nho nhat = " + result1);
-        System.out.println("Gia tri nho nhat = " + result2);
-    }
-
-    // cho integer
-    public static int minFunction(int n1, int n2) {
-        int min;
-        if (n1 > n2) {
-            min = n2;
-        } else {
-            min = n1;
+    public static void main(String args[]) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("args[" + i + "]: "
+                    + args[i]);
         }
-
-        return min;
-    }
-    // cho double
-
-    public static double minFunction(double n1, double n2) {
-        double min;
-        if (n1 > n2) {
-            min = n2;
-        } else {
-            min = n1;
-        }
-
-        return min;
     }
 }
