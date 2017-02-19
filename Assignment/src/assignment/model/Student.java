62d66e8a9dffbca0b19c3102b4fc2425dfb4971d
Donghdd00342 <donghdd00342@fpt.edu.vn>
2017-02-19 23:38:49 +0700

    định nghĩa lớp đối tượng sinh viên.

---
+++ Assignment/src/assignment/model/Student.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment.model;
+
+/**
+ * Class này định nghĩa lớp đối tượng sinh viên
+ * @author DongHo
+ */
+public class Student {
+    String masv;
+    String name;
+    public Student(String masv, String name) {
+        this.masv = masv;
+        this.name = name;
+    }
+    
+}
