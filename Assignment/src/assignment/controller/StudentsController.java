867c71219b40371a5631c4e405580c10cef00739
Donghdd00342 <donghdd00342@fpt.edu.vn>
2017-02-20 00:40:16 +0700

    Xử lý NullPointerException...

--- Assignment/src/assignment/controller/StudentsController.java
+++ Assignment/src/assignment/controller/StudentsController.java
@@ -6,23 +6,24 @@
 package assignment.controller;
 
 import assignment.model.Student;
+import assignment.model.StudentModel;
 import assignment.untils.Untils;
 import assignment.view.View;
-import java.util.Scanner;
 
 /**
  * Class này chứa các hàm thực hiện nhiệm vụ trong menu
+ *
  * @author DongHo
  */
 public class StudentsController {
 
 //    |  1. Tạo mới sinh viên                  |
     /**
-     * Hàm này thực hiện nhiệm vụ thêm mới sinh viên vào cơ sở dữ liệu
-     * Dữ liệu được nhập vào từ người dùng
+     * Hàm này thực hiện nhiệm vụ thêm mới sinh viên vào cơ sở dữ liệu Dữ liệu
+     * được nhập vào từ người dùng
      */
     public static void addStudent() {
-        Student student = null;
+        Student student = new Student();
         boolean continueBoolean = true;
         while (continueBoolean) {
             System.out.println("Nhập vào mã số sinh viên: ");
@@ -31,16 +32,21 @@ public class StudentsController {
             System.out.println("Nhập vào tên sinh viên: ");
             student.setName(Untils.getString(3));
 
-            System.out.println("... chèn dữ liệu vào bảng...");
+            if (StudentModel.save(student)) {
+                System.err.println("Đã thêm mới thành công!");
+            } else {
+                System.err.println("Có gì đó không đúng, vui lòng thử lại...");
+            }
 
             continueBoolean = View.continueBoolean();
         }
 
     }
 //    |  2. Tìm và sửa sinh viên theo mã số    |
+
     /**
-     * Hàm này thực hiện nhiệm vụ sửa sinh viên theo mã số
-     * Người dùng sẽ nhập vào mã số sinh viên để tìm sửa
+     * Hàm này thực hiện nhiệm vụ sửa sinh viên theo mã số Người dùng sẽ nhập
+     * vào mã số sinh viên để tìm sửa
      */
     public static void editStudent() {
         String masv;
@@ -52,27 +58,31 @@ public class StudentsController {
 
     }
 //    |  3. Tìm và xóa sinh viên theo mã số    |
+
     /**
-     * Hàm này thực hiện nhiệm vụ xóa sinh viên theo mã số
-     * Người dùng sẽ nhập vào mã số sinh viên để tìm xóa
+     * Hàm này thực hiện nhiệm vụ xóa sinh viên theo mã số Người dùng sẽ nhập
+     * vào mã số sinh viên để tìm xóa
      */
     public static void deleteStudent() {
         System.out.println("Nhập mã số SV muốn sửa: ");
 
     }
 //    |  4. Tìm thông tin sinh viên theo mã số |
+
     /**
-     * Hàm này thực hiện nhiệm vụ tìm kiếm thông tin sinh viên
-     * Người dùng sẽ nhập vào mã số sinh viên muốn tìm
-     * Nếu tìm thấy chương trình sẽ in ra thông tin.
+     * Hàm này thực hiện nhiệm vụ tìm kiếm thông tin sinh viên Người dùng sẽ
+     * nhập vào mã số sinh viên muốn tìm Nếu tìm thấy chương trình sẽ in ra
+     * thông tin.
      */
     public static void searchStudent() {
         System.out.println("Nhập mã số SV muốn tìm: ");
 
     }
 //    |  5. Hiển thị danh sách sinh viên       |
+
     /**
-     * Hàm này thực hiện nhiệm vụ hiển thị toàn bộ danh sách sinh viên lấy trong CSDL
+     * Hàm này thực hiện nhiệm vụ hiển thị toàn bộ danh sách sinh viên lấy trong
+     * CSDL
      */
     public static void listStudent() {
 
