e7977d98a9230cbf6ddb787f1e61b6421047a2a4
Donghdd00342 <donghdd00342@fpt.edu.vn>
2017-02-20 00:40:28 +0700

    Xử lý NullPointerException...

--- Assignment/src/assignment/model/StudentModel.java
+++ Assignment/src/assignment/model/StudentModel.java
@@ -5,10 +5,35 @@
  */
 package assignment.model;
 
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author DongHo
  */
 public class StudentModel {
-    
+
+    public static boolean save(Student student) {
+        Statement statement;
+        try {
+            statement = DAO.getConnection().createStatement();
+        } catch (SQLException ex) {
+            //Logger.getLogger(StudentModel.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("Có lỗi xảy ra: " + ex);
+            return false;
+        }
+        String string = "INSERT INTO students (masv, name)"
+                + "VALUES ('" + student.getMasv() + "', '" + student.getName() + "');";
+        try {
+            statement.execute(string);
+        } catch (SQLException ex) {
+            //Logger.getLogger(StudentModel.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("Có lỗi xảy ra: " + ex);
+            return false;
+        }
+
+        return true;
+    }
+
 }
