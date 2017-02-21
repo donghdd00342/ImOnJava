751ace0e80d5f4e57de727df75a1d77b944d38d8
Donghdd00342 <donghdd00342@fpt.edu.vn>
2017-02-21 21:34:06 +0700

    model.ProductModel.java

---
+++ UsersDemo/src/model/ProductModel.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import entity.Product;
+
+/**
+ *
+ * @author DongHo
+ */
+public class ProductModel {
+
+    public static Product find(int id) {
+        Product product = new Product();
+
+        return product;
+    }
+
+    public static int insert(Product product) {
+        int id = 0;
+
+        return id;
+
+    }
+
+    public static boolean update(Product product, int id) {
+        boolean success = true;
+
+        return success;
+    }
+
+    public static Product delete(int id) {
+        Product product = new Product();
+
+        return product;
+    }
+
+    public static void all(byte pagination) {
+
+    }
+
+}
