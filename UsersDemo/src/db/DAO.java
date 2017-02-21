a2b68605cff3f061531e5bb4bb57bb9bf8261f4b
Donghdd00342 <donghdd00342@fpt.edu.vn>
2017-02-21 21:32:16 +0700

    DAO.java

---
+++ UsersDemo/src/db/DAO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author DongHo
+ */
+public class DAO {
+
+    public static Connection getConnection() {
+//        return DriverManager.getConnection(urlConnection, string1, string2);
+        return null;
+    }
+}
