059d930a4dc3dd7421f55437c3c93dab8c4cfb1f
Donghdd00342 <donghdd00342@fpt.edu.vn>
2017-03-05 12:45:48 +0700

    ArrayList...

--- iTest/src/Collection/ArrayListTest.java
+++ iTest/src/Collection/ArrayListTest.java
@@ -5,6 +5,7 @@
  */
 package Collection;
 
+import java.sql.Array;
 import java.util.ArrayList;
 
 /**
@@ -14,49 +15,71 @@ import java.util.ArrayList;
 class ArrayListDemo {
 
      public static void main(String args[]) {
-	  // tạo ArrayList<Integer> trống
-	  ArrayList<Integer> listInt = new ArrayList<>();
-	  // thêm 5 phần tử vào List
-	  System.err.println("- size() = "+ listInt.size());
-	  listInt.add(1);
-	  listInt.add(1, 3);
-	  listInt.add(5);
-	  listInt.add(10);
-	  // kiểm tra Empty
-	  System.err.println("- size() = "+ listInt.size());
-	  System.out.println(".isEmpty() = " + listInt.isEmpty()); // false
-	  System.out.println(".contains(10) = " + listInt.contains(10)); // true
-	  System.out.println(".toString() = " + listInt.toString()); // [1, 3, 5, 10]
-	  // in ra màn hình với foreach thông thường
-	  System.out.println("----------- bắt đầu in ---------------");
-	  for (int in : listInt) {
-	       System.out.println(in);
-	  }
+//	  // tạo ArrayList<Integer> trống
+//	  ArrayList<Integer> listInt = new ArrayList<>();
+//	  // thêm 5 phần tử vào List
+//	  System.err.println("- size() = "+ listInt.size());
+//	  listInt.add(1);
+//	  listInt.add(1, 3);
+//	  listInt.add(5);
+//	  listInt.add(10);
+//	  // kiểm tra Empty
+//	  System.err.println("- size() = "+ listInt.size());
+//	  System.out.println(".isEmpty() = " + listInt.isEmpty()); // false
+//	  System.out.println(".contains(10) = " + listInt.contains(10)); // true
+//	  System.out.println(".toString() = " + listInt.toString()); // [1, 3, 5, 10]
+//	  // in ra màn hình với foreach thông thường
+//	  System.out.println("----------- bắt đầu in ---------------");
+//	  for (int in : listInt) {
+//	       System.out.println(in);
+//	  }	  
+//	  System.out.println("----------- bắt đầu đổi giá trị ---------------");
+//	  // đổi giá trị một phần tử
+//	  listInt.set(0, 100);
+//	  // in ra màn hình
+//	  listInt.forEach((integer) -> {
+//	       System.out.println(integer);
+//	  });
+//	  System.err.println("- size() = "+ listInt.size());
+//	  // xóa một phân tử
+//	  System.out.println("----------- bắt đầu xóa phần tử ---------------");
+//	  listInt.remove(Integer.valueOf("50"));
+//	  // in ra màn hình
+//	  for (int i = 0; i < listInt.size(); i++) {
+//	       System.out.println(listInt.get(i));	       
+//	  }
+//	  // xóa một phân tử
+//	  System.err.println("- size() = "+ listInt.size());
+//	  System.out.println("----------- Xóa phần tử tiếp ---------------");
+//	  listInt.remove(listInt.size()-1);
+//	  // in
+//	  listInt.forEach((t) -> {
+//	       System.out.println(t);
+//	  });
+//	  System.err.println("- size() = "+ listInt.size());
+//	  
+/////////////////////////////////////////////////////////////////////////////////
+	  System.err.println("------------------------------------------------------------------");
 	  
-	  System.out.println("----------- bắt đầu đổi giá trị ---------------");
-	  // đổi giá trị một phần tử
-	  listInt.set(0, 100);
+	  ArrayList arrL = new ArrayList();
+	  System.err.println("- size() = "+ arrL.size());
+	  arrL.add(1);
+	  arrL.add("2");
+	  arrL.add('9');
+	  arrL.add("Dong");
+	  arrL.add('G');
+	  System.err.println("- size() = "+ arrL.size());
 	  // in ra màn hình
-	  listInt.forEach((integer) -> {
-	       System.out.println(integer);
-	  });
-	  System.err.println("- size() = "+ listInt.size());
-	  // xóa một phân tử
-	  System.out.println("----------- bắt đầu xóa phần tử ---------------");
-	  listInt.remove(Integer.valueOf("50"));
-	  // in ra màn hình
-	  for (int i = 0; i < listInt.size(); i++) {
-	       System.out.println(listInt.get(i));
-	       
-	  }
-	  // xóa một phân tử
-	  System.err.println("- size() = "+ listInt.size());
-	  System.out.println("----------- Xóa phần tử tiếp ---------------");
-	  listInt.remove(listInt.size()-1);
-	  // in
-	  listInt.forEach((t) -> {
+	  arrL.forEach((t) -> {
 	       System.out.println(t);
 	  });
-	  System.err.println("- size() = "+ listInt.size());
+	  // xóa
+	  arrL.remove(2);
+	  arrL.remove(1);
+	  // in
+	  arrL.forEach((t) -> {
+	       System.out.println(t);
+	  });
+	  System.err.println("- size() = "+ arrL.size());
      }
 }
